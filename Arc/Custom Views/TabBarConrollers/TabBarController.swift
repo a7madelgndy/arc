c07ad649bc63e2fd3bde465f822a3bff7a6c5ebc
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-19 15:09:01 +0200

    set up tabBarConroller With Discover and Favorits Tabs

---
+++ Arc/Custom Views/TabBarConrollers/TabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  ArcTabBarController.swift
+//  Arc
+//
+//  Created by Ahmed El Gndy on 19/04/2025.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [createDiscoverNC(), CreateFavoritesNC()]
+        UITabBar.appearance().tintColor = .systemGreen
+    }
+    
+    
+    func createDiscoverNC() -> UINavigationController {
+        let DiscoverViewController =  ViewController()
+        DiscoverViewController.title = "Discover"
+        DiscoverViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 0)
+        
+        return UINavigationController(rootViewController: DiscoverViewController)
+    }
+    
+    
+    func CreateFavoritesNC() ->UINavigationController {
+        let favoritsVC = ViewController()
+        favoritsVC.title = "Favorites"
+        favoritsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritsVC)
+    }
+}
