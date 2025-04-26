7dcfdb0695486bd5b620ad16d2fb6825fcc88180
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-26 03:05:54 +0300

    adding chevron.right SFymobl to to the CastView  to improve user Experince

--- Arc/Screens/Movie Detilas  Screen/views/castView.swift
+++ Arc/Screens/Movie Detilas  Screen/views/castView.swift
@@ -11,29 +11,52 @@ class MovieCastView: UIView, UICollectionViewDelegate {
     var actors :[CastMember]? {
         didSet {
             collectionView.reloadData()
+            configuerChevronSF()
         }
     }
-    
+    private var rightchevronSF : SFSymbolImageView!
     private var collectionView:UICollectionView!
+    
+    private var framewidth : CGFloat = UIScreen.main.bounds.width
+    private var didNotScrolled = true
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureCollectoinView()
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     
     func configureCollectoinView() {
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: UIHelper.createFourColumnFlowLayout(in: self))
         collectionView.delegate = self
         collectionView.dataSource = self
+        
         collectionView.showsHorizontalScrollIndicator = false
+        
         collectionView.register(ActorCell.self , forCellWithReuseIdentifier: ActorCell.reuseIdentifier)
         
         addSubview(collectionView)
-        collectionView.pinToEages(to: self)
+    }
+    
+    func configuerChevronSF() {
+ 
+        if actors?.count ?? 0 >= 3 {
+            rightchevronSF = SFSymbolImageView(systemImage: "chevron.right", tintColor: .systemPurple.withAlphaComponent(0.4))
+            
+            addSubview(rightchevronSF)
+            collectionView.setConstrains(top: topAnchor , leading:  leadingAnchor , bottom: bottomAnchor)
+            collectionView.setWidth(width: framewidth * 0.85)
+            
+            rightchevronSF.setConstrains(top: topAnchor , leading:  collectionView.trailingAnchor , bottom: bottomAnchor, trailing: trailingAnchor)
+        }else {
+            collectionView.pinToEages(to: self)
+        }
+   
     }
 }
 
@@ -42,9 +65,12 @@ extension MovieCastView: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         1
     }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return actors?.count ?? 0
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ActorCell.reuseIdentifier, for: indexPath) as? ActorCell else {
@@ -55,4 +81,14 @@ extension MovieCastView: UICollectionViewDataSource {
         return cell
     }
     
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if didNotScrolled {
+            rightchevronSF.removeFromSuperview()
+            let width:Float = Float(UIScreen.main.bounds.width )
+            collectionView.setWidth(width: CGFloat(width))
+            didNotScrolled.toggle()
+        }
+    }
+
 }
